9d6af20b3c87d798a7501e8fad697df49d0fefce
Franco Camporeale <camporeale@gmail.com>
2019-09-19 01:24:30 +0000

    :

--- full_pipeline.py
+++ full_pipeline.py
@@ -1,58 +1,30 @@
-import pandas as pd
-import numpy as np
-import fasttext
-from sklearn.metrics import balanced_accuracy_score
 from multiprocessing import  Pool
-from utils import preprocess, preprocess_test, parallelize
+from utils import prepare_data, train_models
 
-def prepare_data():
-    # Load Dataset
-    data_train = pd.read_csv('../data/train.csv')
-    data_test = pd.read_csv('../data/test.csv')
+# Files provided by MercadoLibre 
+data_files = {'train':'../data/train.csv', 'test': '../data/test.csv'}
 
-    # Preprocess data
-    train = parallelize_dataframe(data_train, preprocess)
-    test = parallelize_dataframe(data_train, preprocess_test)
+# Names of files created by the preprocessing steps
+normalized_files = {'train': '../data/train_full_norm.csv', 
+                    'validation': '../data/val_full_norm.csv', 
+                    'test': '../data/test_full_norm.csv'}
 
-    # Create full splits
-    full_csv_names = ['../data/train_full_norm.csv','../data/val_full_norm.csv','../data/test_full_norm.csv']
-    create_files(train, test, full_csv_names)
+normalized_language_files = {'spanish':['../data/train_spanish_norm.csv','../data/val_spanish_norm.csv','../data/test_spanish_norm.csv'], 
+                             'portuguese': ['../data/train_portuguese_norm.csv','../data/val_portuguese_norm.csv','../data/test_portuguese_norm.csv']}
 
-    # Create train, val and test by language
-    language_csv_names = {'spanish':['../data/train_spanish_norm.csv','../data/val_spanish_norm.csv','../data/test_spanish_norm.csv'] 
-                        'portuguese': ['../data/train_portuguese_norm.csv','../data/val_portuguese_norm.csv','../data/test_portuguese_norm.csv']}
-    create_language_files(train, test, language_csv_names)
+normalized_reliable_files = {'train': '../data/train_reliable_norm.csv', 
+                             'validation': '../data/val_reliable_norm.csv'}
 
-    # Create df with reliable labels oversampling
-    reliable_files = ['../data/train_reliable_norm.csv','../data/val_reliable_norm.csv']
-    create_reliable_df(train, reliable_files)
-    
-def train_models():
-    # Train simple model
-    model1 = fasttext.train_supervised(input="../data/train_fasttext_norm.csv", epoch=5, lr=0.5, wordNgrams=2, thread=8, dim=200)
-    model2 = 
-    model3 = 
-    model4 =
-    model5 = 
+submission_file = 'submission.csv'
 
-    model.save_model("../models/model.bin")
+# Preprocessing data and creating files
+prepare_data(data_files,normalized_files, normalized_language_files, normalized_reliable_files)
+          
+# Training all models
+#train_models(ncpu=8)
 
+# Make ensemble prediction
+#make_ensemble_prediction()
 
-def evaluate_model(model):
-    model.test('../data/val_fasttext.csv')
-
-                
-def run_parallel_ensemble(model_files, n_cores=5):
-    results = Parallel(n_jobs=n_cores)(delayed(run_model_test)(model) for model in model_files)
-    results_df = pd.concat([x for x in results], axis=1)
-    voted_results = {"id": [], "category": []}
-
-    return results    
-    
-    
-
-test_data = pd.read_csv('../data/test_fasttext_norm.csv',names=['tokens'])
-test_data.replace(np.nan, 'notitle',inplace=True)
-predictions = model.predict(test_data["tokens"].values.tolist())
-submission = pd.Series([x[0][9:] for x in predictions[0]])
-submission.to_csv("./submissions/submission_test.csv",header=["category"],index_label="id")
+# creating submission file
+#make_submission(submission_file)
--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ from spacy.lang.es import Spanish
 from spacy.lang.pt import Portuguese
 from spacy.tokenizer import Tokenizer
 import unicodedata
+from multiprocessing import  Pool
+import fasttext
+import numpy as np
+import csv
 
 def normalize_text(text,nlp):
     s = []
@@ -20,13 +24,16 @@ def normalize_text(text,nlp):
         s = ' '.join(s)
         return s
 
+    
 def parallelize_dataframe(df, func, n_cores=8):
+    print("Executing parallel ", func.__name__)
     df_split = np.array_split(df, n_cores)
     pool = Pool(n_cores)
     df = pd.concat(pool.map(func, df_split))
     pool.close()
     pool.join()
     return df
+
 
 def preprocess(df):
     # Spacy Tokenizers
@@ -41,6 +48,7 @@ def preprocess(df):
     df["label"] = df["category"].apply(lambda x: '__label__'+ x)
     return df
 
+
 def preprocess_test(df):
     # Spacy Tokenizers
     nlp_es = Spanish()
@@ -53,41 +61,79 @@ def preprocess_test(df):
     # Test file only needs id and tokens
     return df
 
+
 def create_files(train_df, test_df, csv_names): 
+    print("Creating files")
     # Create Train and validation full files
-    X_train, X_val, y_train, y_val = train_test_split(train["tokens"], train["label"], test_size=0.05, random_state=42, stratify=train_df["label"])
+    X_train, X_val, y_train, y_val = train_test_split(train_df["tokens"], train_df["label"], test_size=0.05, random_state=42, stratify=train_df["label"])
     train_norm = pd.concat([y_train,X_train], axis=1)
     val_norm = pd.concat([y_val,X_val], axis=1)
     
     # Saving as fasttext format
-    train_norm.to_csv(csv_names[0],index=False, sep=' ', header=False, quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")
-    val_norm.to_csv(csv_names[1],index=False, sep=' ', header=False, quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")   
-    print("Training: ")
+    train_norm.to_csv(csv_names["train"],index=False, sep=' ', header=False, quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")
+    val_norm.to_csv(csv_names["validation"],index=False, sep=' ', header=False, quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")   
     
     # Test set file
     if test_df:
         print("Processing Test file")
-        test["tokens"].to_csv(csv_names[2],index=False,header=False,line_terminator='\n')
+        test_df["tokens"].to_csv(csv_names["test"],index=False,header=False,line_terminator='\n')
         
 
 def create_language_files(train_df, test_df, csv_names):
-    spanish_df = data_train[data_train["language"] == 'spanish']
+    spanish_train = data_train[data_train["language"] == 'spanish']
     spanish_test = data_test[data_test["language"] == 'spanish']
-    create_files(spanish_df, spanish_test, csv_names['spanish'])
-        
-    portuguese_df = data_train[data_train["language"] == 'portuguese']
-    portuguese_test = data_test[data_test["language"] == 'portuguese']
-    create_files(portuguese_df, portuguese_test, csv_names["portuguese"])
+    create_files(spanish_train, spanish_test, csv_names['spanish'])
     
+    portuguese_train = data_train[data_train["language"] == 'portuguese']
+    portuguese_test = data_test[data_test["language"] == 'portuguese']
+    create_files(portuguese_train, portuguese_test, csv_names["portuguese"])
+    
+    spanish_test["language"].to_csv('../data/language_mapping_spanish.csv', index=False, header=False)
+    portuguese_test["language"].to_csv('../data/language_mapping_portuguese.csv', index=False, header=False)
 
+    
 def create_reliable_files(train_df, csv_names):
     rlabel = train_df[data_train["label_quality"] == 'reliable']
-    urlabel = data_train[data_train["label_quality"] == 'unreliable'].sample(1184245)
+    urlabel = data_train[data_train["label_quality"] == 'unreliable'].sample(n=1184245, random_state=42)
     data = pd.concat([rlabel,urlabel])
     create_files(portuguese_df, portuguese_test, csv_names["portuguese"])
 
-        
-def predict_test(model):
+    
+def prepare_data(data_files, normalized_files, normalized_language_files, normalized_reliable_files):
+    print("Preparing data")
+    # Load Dataset
+    data_train = pd.read_csv(data_files['train'])
+    data_test = pd.read_csv(data_files['test'])
+
+    # Preprocess data
+    train = parallelize_dataframe(data_train, preprocess)
+    test = parallelize_dataframe(data_test, preprocess_test)
+
+    # Create full splits
+    create_files(train, test, normalized_files)
+
+    # Create train, val and test by language
+    create_language_files(train, test, normalized_language_files)
+
+    # Create df with reliable labels oversampling
+    reliable_files = ['../data/train_reliable_norm.csv','../data/val_reliable_norm.csv']
+    create_reliable_df(train, reliable_files)
+
+    
+def train_models(ncpu=8):
+    models = {"model_full": {"file": "../data/train_fasttext_norm.csv", "epoch": 5, "lr":0.5, "wordNgrams":2, "dim":100} ,
+              "model_full_15epochs": {"file": "../data/train_fasttext_norm.csv", "epoch": 15, "lr":0.5, "wordNgrams":2, "dim":100},
+              "model_full_3gram": {"file": "../data/train_fasttext_norm.csv", "epoch": 5, "lr":0.5, "wordNgrams":3, "dim":100},
+              "model_reliable": {"file": "../data/train_reliable_norm.csv", "epoch": 5, "lr":0.5, "wordNgrams":2, "dim":200 },
+              "model_spanish": {"file": "../data/train_spanish_norm.csv", "epoch": 5, "lr":0.5, "wordNgrams":2, "dim":200 },
+              "model_portuguese": {"file": "../data/train_portuguese_norm.csv", "epoch": 5, "lr":0.5, "wordNgrams":2, "dim":200 }}
+    for model in models:
+        output = fasttext.train_supervised(input=model["file"], epoch=model["epoch"], lr=model["lr"],
+                                           wordNgrams=model["wordNgrams"], dim=model["dim"], thread=ncpu)
+        output.save_model('../models/' + model)
+
+    
+def make_ensemble_prediction(model):
     test_data = pd.read_csv(test_df,header=None,names=['title'])
     test_data['title'] = test_data['title'].apply(lambda x: ' '.join(x.split()[1:]))
     
@@ -114,3 +160,19 @@ def predict_test(model):
         predictions = model.predict(val_data["title"].values.tolist())
         print("Predict finished for model ", model)
         return pd.Series([x[0][9:] for x in predictions[0]])
+
+    
+def parallel_models_get_test_results(model_files, n_cores=5):
+    results = Parallel(n_jobs=n_cores)(delayed(run_model_test)(model) for model in model_files)
+    return results
+
+def calculate_results(results):
+    voted_results = {"id": [], "category": []}
+    results_df = pd.concat([x for x in results], axis=1)
+    for index, row in results_df.iloc[:,1:].iterrows():
+        voted_results["id"].append(index)
+        voted_results["category"].append(row.value_counts().index[0])
+
+    voted_results_df = pd.DataFrame.from_dict(voted_results)
+    return voted_results_df
+    print("Finished")
